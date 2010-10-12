eaea79c0b667fc3162738ea8ec92acc112ed65e9
Kriti Godey <kriti.godey@gmail.com>
2010-10-12 00:01:13 -0400

    Formset ajax

---
+++ recipes/media/recipes/forms.js
@@ -0,0 +1,20 @@
+function cloneMore(selector, type) {
+    var newElement = $(selector).clone(true);
+    var total = $('#id_' + type + '-TOTAL_FORMS').val();
+    newElement.find(':input').each(function() {
+        var name = $(this).attr('name').replace('-' + (total-1) + '-','-' + total + '-');
+        var id = 'id_' + name;
+
+        if ($(this).attr('type') != 'hidden') {
+            $(this).val('');
+        }
+        $(this).attr({'name': name, 'id': id}).removeAttr('checked');
+    });
+    newElement.find('label').each(function() {
+        var newFor = $(this).attr('for').replace('-' + (total-1) + '-','-' + total + '-');
+        $(this).attr('for', newFor);
+    });
+    total++;
+    $('#id_' + type + '-TOTAL_FORMS').val(total);
+    $(selector).after(newElement);
+}
