2e6d077d46ecb13c799b631311df23a8538766c6
Kriti Godey <kriti.godey@gmail.com>
2010-10-17 13:51:51 -0400

    fixed typo

--- recipes/urls.py
+++ recipes/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
 	url(r'^junk/$', views.all_junk_recipes, name='all_junk_recipes'),
 	url(r'^edit/(?P<id>\d+)/$', views.edit_recipe, name='edit_recipe'),
 	url(r'^login/$', views.login, name='login'),
-	url(r'^users/(?P<username>\d+)/$', views.userpage, name='userpage')
+	url(r'^users/(?P<username>\d+)/$', views.userpage, name='userpage'),
 	url(r'^(?P<id>\d+)/$', views.view_recipe, name='view_recipe'),
 )
