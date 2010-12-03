49578914e330453a6dff60fe53e58af61d176207
Kriti Godey <kriti.godey@gmail.com>
2010-12-03 01:45:10 -0500

    Enabled inline admin for NutrientValue

--- nutrition/admin.py
+++ nutrition/admin.py
@@ -19,7 +19,7 @@ class FoodAdmin(admin.ModelAdmin):
 	list_display = ('description', 'short_description', 'scientific_name', 'food_group', 'ndb_number')
 	list_filter = ('food_group', 'manufacturer', 'fndds_profile')
 	search_fields = ('description', 'short_description', 'scientific_name')
-#	inlines = [NutrientValueInline, WeightInline, FootnoteInline]
+	inlines = [NutrientValueInline] #WeightInline, FootnoteInline]
 #	the above line is commented out because this will cause the server to run out of memory :\
 #	in other words, don't uncomment it
 
