b642721bad68d724e07720ff721db84d7be4d614
Kriti Godey <kriti.godey@gmail.com>
2011-03-03 11:41:06 +0000

    Registered the new model with reversion and the admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Ingredient, MeasurementUnit, Recipe, RecipeIngredient, JunkRecipe, Photo, UserRecipeRating, UserIngredientRating, PantryItem, KitchenTool, RecipeKitchenTool
+from .models import Ingredient, MeasurementUnit, Recipe, RecipeIngredient, JunkRecipe, Photo, UserRecipeRating, UserIngredientRating, PantryItem, KitchenTool, RecipeKitchenTool, UserSavedRecipe
 from reversion.admin import VersionAdmin
 
 class RecipeAdmin(VersionAdmin):
 	pass
 
-admin.site.register((Ingredient, MeasurementUnit, RecipeIngredient, JunkRecipe, Photo, UserRecipeRating, UserIngredientRating, PantryItem, KitchenTool, RecipeKitchenTool), VersionAdmin)
+admin.site.register((Ingredient, MeasurementUnit, RecipeIngredient, JunkRecipe, Photo, UserRecipeRating, UserIngredientRating, PantryItem, KitchenTool, RecipeKitchenTool, UserSavedRecipe), VersionAdmin)
 admin.site.register(Recipe, RecipeAdmin)
