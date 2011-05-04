56092f172629d41d621ba919b0883c4d6e393cd6
Kriti Godey <kriti.godey@gmail.com>
2011-05-04 06:46:36 +0000

    Deleted recommendations directory.y

--- recipes/recommendations/freyne.py
+++
@@ -1,5 +0,0 @@
-from recipes.models import Recipe, Ingredient, UserIngredientRating, UserRecipeRating
-from django.contrib.auth.models import User
-
-# Implementing the recommendation systems in the
-# Freyne paper
