6320e291604d002e64d13fbe50ac90d2423a4bfc
Kriti Godey <kriti@kritigodey.com>
2012-02-26 23:41:45 -0500

    Added ingredient and unit models, with recipe ingredients to keep track of ingredients in recipes and user ingredient ratings. Modified recipes to not take ingredients.

--- bendakai/models.py
+++ bendakai/models.py
@@ -3,6 +3,38 @@ from django.contrib.auth.models import User
 import datetime
 from taggit.managers import TaggableManager
 
+class Ingredient(models.Model):
+	"""
+	A class that models an ingredient (independent of recipe.)
+	
+	"""
+
+	name = models.CharField(max_length=255)
+	slug = models.SlugField()
+	# Ingredients that should be considered the same as this one.
+	equivalent_ingredients = models.ManyToManyField('self', blank=True, null=True)
+
+	class Meta:
+		ordering = ['name']
+
+	def __unicode__(self):
+		return self.name
+
+
+class MeasurementUnit(models.Model):
+	"""
+	A class that models a unit (independent of recipe.)
+	
+	"""
+
+	name = models.CharField(max_length=255)
+	slug = models.SlugField()
+	# Units that should be considered the same as this one.
+	equivalent_units = models.ManyToManyField('self', blank=True, null=True)
+
+	def __unicode__(self):
+		return self.name
+
 
 class Recipe(models.Model):
 	"""
@@ -12,7 +44,6 @@ class Recipe(models.Model):
 	title = models.CharField(max_length=255)
 	slug = models.SlugField()
 	description = models.TextField(blank=True, null=True)
-	ingredients = models.TextField()
 	directions = models.TextField()
 	servings = models.IntegerField(blank=True, null=True)
 	prep_time = models.IntegerField(blank=True, null=True)
@@ -28,6 +59,37 @@ class Recipe(models.Model):
 	
 	def __unicode__(self):
 		return self.title
+
+
+class RecipeIngredient(models.Model):
+	"""
+	A class that maps recipes to ingredients.
+	
+	"""
+	recipe = models.ForeignKey(Recipe, related_name='ingredients')
+	ingredient = models.ForeignKey(Ingredient, related_name='recipes', help_text='If entering things like "large onion", put "large" under description.')
+	min_quantity = models.CharField(max_length=255, null=True, blank=True, help_text='Takes fractions or decimals. Decimals below 1 must be in the form "0.x", not ".x"')
+	max_quantity = models.FloatField(null=True, blank=True, help_text='For ranges of quantities e.g. "2-3 tsp".')
+	unit = models.ForeignKey(MeasurementUnit, null=True, blank=True)
+	description = models.CharField(max_length=255, blank=True, null=True, help_text='Things like "diced", "large", "pre-cooked", "ripe" etc. go here.')
+	optional = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return '%s (%s)'%(self.ingredient.name, self.recipe.name)
+
+
+
+class UserIngredientRating(models.Model):
+	"""
+	A class that stores user ratings for ingredients.
+	"""
+
+	user = models.ForeignKey(User)
+	ingredient = models.ForeignKey(Ingredient, related_name='ratings')
+	rating = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return '%s rates %s %d stars'%(self.user.username, self.ingredient.name, self.rating)
 
 
 class UserRecipeRating(models.Model):
